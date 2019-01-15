d27d80106c4abdaec7c4a759700ca66eeade0a76
尹怡然 <yinyiran100@163.com>
2019-01-15 18:05:50 +0800

    webpack4 init

---
+++ Webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+module.exports = {
+  entry: { main: "./src/index.js" },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js"
+  },
+  target: "node",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: "/node_modules/",
+        use: {
+          loader: "balel-loader"
+        }
+      },
+      {
+        test:/\.css$/,
+        use
+      }
+    ]
+  }
+};
---
+++ src/index.js
@@ -0,0 +1,2 @@
+import "./style.css"
+console.log("Hello world From webpack4")
