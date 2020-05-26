6f1b6733728e3fdccc0aaa62dd923ae33450156b
chrisgad <chrisgad@qq.com>
2020-05-26 18:04:35 +0800

    editor

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,9 +1,39 @@
 <template>
-  <div>https://juejin.im/post/5d8342cd6fb9a06b1027624d</div>
+  <div>
+    <editor api-key="no-api-key" :init="editConfig" />
+  </div>
 </template>
 
 <script>
-  export default {};
+  import Tinymce from "@tinymce/tinymce-vue";
+
+  export default {
+    components: {
+      editor: Tinymce
+    },
+    data() {
+      const Id = Date.now();
+      return {
+        Id: Id,
+        editConfig: {
+          height: 500,
+          menubar: false,
+          plugins: [
+            "advlist autolink lists link image charmap print preview anchor",
+            "searchreplace visualblocks code fullscreen",
+            "insertdatetime media table paste code help wordcount"
+          ],
+          toolbar:
+            "undo redo | formatselect | bold italic backcolor | \
+               alignleft aligncenter alignright alignjustify | \
+               bullist numlist outdent indent | removeformat | help"
+        }
+      };
+    },
+    props: {},
+    mounted() {},
+    methods: {}
+  };
 </script>
 
 <style>
--- src/manage/component/MArticles.vue
+++ src/manage/component/MArticles.vue
@@ -38,13 +38,16 @@
         </div>
       </el-form>
     </el-dialog>
+    <editor></editor>
   </div>
 </template>
 
 <script>
   import { HTTP } from "../../service";
   import { MessageBox, Message } from "element-ui";
+  import Editor from "../../components/Editor";
   export default {
+    components: { Editor },
     data() {
       return {
         articles: [],
