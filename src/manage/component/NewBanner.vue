88a55b4abee5a0be8014e95e5332b72388319d34
chrisgad <chrisgad@qq.com>
2020-06-16 23:05:08 +0800

    Banner 新增

---
+++ src/manage/component/NewBanner.vue
@@ -0,0 +1,119 @@
+<template>
+  <el-dialog
+    :title="form.BanID?`编辑轮播图`:'新增轮播图'"
+    :visible.sync="visible"
+    :before-close="cancelAdd"
+    width="800px"
+    :append-to-body="true"
+  >
+    <el-form :model="form" label-width="100px" size="small">
+      <el-form-item label="轮播名称">
+        <el-input v-model="form.BanName"></el-input>
+      </el-form-item>
+      <el-form-item label="轮播类型">
+        <el-select v-model="form.BanType" placeholder="请选择类型" @change="changeType">
+          <el-option
+            v-for="item in typeOptions"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item :label="typeText">
+        <el-select v-model="form.BanTargID" :placeholder="`请选择${typeText}`">
+          <el-option
+            v-for="item in targOptions"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="图片">
+        <upload-file :imgs="form.BanImg" ref="UpFileRef"></upload-file>
+      </el-form-item>
+    </el-form>
+    <div class="footer">
+      <el-button size="small" @click="cancelAdd">取消</el-button>
+      <el-button size="small" type="primary" @click="saveBan">保存</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+  import UploadFile from "../../components/UploadFile";
+  import { HTTP } from "../../service";
+  import { Message } from "element-ui";
+  export default {
+    props: {
+      form: Object,
+      visible: Boolean
+    },
+    components: { UploadFile },
+    computed: {
+      typeText() {
+        return this.form.BanType == 1 ? "链接的产品" : "链接的新闻";
+      }
+    },
+    created() {},
+    data() {
+      return {
+        typeOptions: [
+          { label: "产品", value: 1 },
+          { label: "新闻", value: 2 }
+        ],
+        targOptions: [],
+        cacheOptions: {}
+      };
+    },
+    watch: {
+      visible(val) {
+        if (val) this.getTargList();
+      }
+    },
+    methods: {
+      changeType() {
+        this.form.BanTargID = null;
+        this.getTargList();
+      },
+      getTargList() {
+        let typeNum = this.form.BanType;
+        if (this.cacheOptions[typeNum]) {
+          this.targOptions = this.cacheOptions[typeNum];
+        } else {
+          let url = typeNum == 1 ? "/getProducts" : "/getArticle";
+          HTTP.get(url).then(res => {
+            this.targOptions = res.data.map(item => {
+              if (typeNum === 1) {
+                return { label: item.ProdName, value: item.ProdID };
+              } else {
+                return { label: item.ArtTitle, value: item.ArtID };
+              }
+            });
+            this.cacheOptions[typeNum] = this.targOptions;
+          });
+        }
+      },
+      cancelAdd() {
+        this.$emit("update:visible", false);
+      },
+      async saveBan() {
+        let img = await this.$refs.UpFileRef.upload();
+        let { BanName, BanTargID, BanType } = this.form;
+        let params = { BanImg: img[0], BanName, BanTargID, BanType };
+        HTTP.post("/saveBanner", params).then(res => {
+          Message.success("保存成功");
+          let Obj = Object.assign({ BanID: res.data.insertId }, params);
+          this.$emit("saveSuccess", Obj);
+        });
+      }
+    }
+  };
+</script>
+
+<style lang="less" scoped>
+  .footer {
+    text-align: right;
+  }
+</style>
--- src/manage/page/MBanner.vue
+++ src/manage/page/MBanner.vue
@@ -6,10 +6,10 @@
     </div>
     <el-table :data="banners">
       <el-table-column label="序号" type="index" width="50px" align="center"></el-table-column>
+      <el-table-column label="轮播名称" prop="BanName"></el-table-column>
       <el-table-column label="图片">
         <img class="banner-img" slot-scope="{row}" :src="row.BanImg" />
       </el-table-column>
-      <el-table-column label="备注" prop="BanIntro"></el-table-column>
       <el-table-column label="类型" prop="BanType" width="50px" align="center"></el-table-column>
       <el-table-column label="操作" width="100px" align="center">
         <span slot-scope="{row,$index}">
@@ -18,50 +18,7 @@
         </span>
       </el-table-column>
     </el-table>
-    <el-dialog
-      :title="form.BanID?`编辑`:'新增'"
-      :visible.sync="isCreate"
-      :before-close="cancelAdd"
-      width="800px"
-      :append-to-body="true"
-    >
-      <el-form :model="form" label-width="80px" size="small">
-        <el-form-item label="名称">
-          <el-input v-model="form.BanName"></el-input>
-        </el-form-item>
-        <el-form-item label="类型">
-          <el-select v-model="form.BanType" placeholder="请选择类型" @change="changeType">
-            <el-option
-              v-for="item in typeOptions"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            ></el-option>
-          </el-select>
-          <el-select
-            v-model="form.BanTargID"
-            placeholder="请选择产品"
-            filterable
-            remote
-            :remote-method="getTargList"
-          >
-            <el-option
-              v-for="item in targOptions"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            ></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="图片">
-          <upload-file :imgs="form.BanImg" ref="UpFileRef"></upload-file>
-        </el-form-item>
-      </el-form>
-      <div class="footer">
-        <el-button size="small" @click="cancelAdd">取消</el-button>
-        <el-button size="small" type="primary" @click="saveBan">保存</el-button>
-      </div>
-    </el-dialog>
+    <new-banner :visible.sync="isCreate" :form="form" @saveSuccess="saveSuccess"></new-banner>
     <sort :visible.sync="isSort" />
   </div>
 </template>
@@ -69,11 +26,11 @@
 <script>
   import { HTTP } from "../../service";
   import { ServeHost } from "../../service/util";
-  import UploadFile from "../../components/UploadFile";
+  import NewBanner from "../component/NewBanner";
   import Sort from "../../components/Sort";
   import { Message, MessageBox } from "element-ui";
   export default {
-    components: { UploadFile, Sort },
+    components: { Sort, NewBanner },
     created() {
       HTTP.get("/getBanner").then(res => {
         res.data.forEach(item => {
@@ -86,11 +43,6 @@
       return {
         banners: [],
         form: {},
-        typeOptions: [
-          { label: "产品", value: 1 },
-          { label: "新闻", value: 2 }
-        ],
-        targOptions: [],
         isCreate: false,
         isSort: false
       };
@@ -113,19 +65,6 @@
           }
         );
       },
-      changeType() {
-        this.targOptions = [];
-      },
-      getTargList() {
-        let url = this.form.BanType == 1 ? "/getProducts" : "/getArticle";
-        HTTP.get(url).then(res => {
-          this.targOptions = res.data.map(item => {
-            let label = this.form.BanType === 1 ? item.ProdName : item.ArtTitle;
-            let value = this.form.BanType === 1 ? item.ProdID : item.ArtID;
-            return { label, value };
-          });
-        });
-      },
       createBan() {
         this.form = {
           BanImg: [],
@@ -135,17 +74,10 @@
         };
         this.isCreate = true;
       },
-      cancelAdd() {
-        this.isCreate = false;
-      },
-      async saveBan() {
-        let img = await this.$refs.UpFileRef.upload();
-        let { BanName, BanTargID, BanType } = this.form;
-        let params = { BanImg: img, BanName, BanTargID, BanType };
-        HTTP.post("/saveBanner", params).then(res => {
-          Message.success("保存成功");
-          this.isCreate = false;
-        });
+      saveSuccess(val) {
+        console.log(val)
+        this.banners.push(val);
+        this.isCreate = false
       }
     }
   };
@@ -159,8 +91,5 @@
     width: 100%;
     height: 40px;
     object-fit: cover;
-  }
-  .footer {
-    text-align: right;
   }
 </style>
