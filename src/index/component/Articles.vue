abe787d5854da9325dad367566bcf4942459de54
chrisgad <chrisgad@qq.com>
2020-07-10 23:00:42 +0800

    products list and article list

--- src/index/component/Articles.vue
+++ src/index/component/Articles.vue
@@ -1,13 +1,33 @@
 <template>
-  <div>文章</div>
+  <div>
+    <div class="class-item" v-for="art in articls" :key="art.ArtID">
+      <img width="100px" height="100px" :src="art.ArtCover" alt />
+      <span>{{art.ArtTitle}}</span>
+      <span>{{art.ArtIntro}}</span>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+  import { Data } from "../../service";
+  export default {
+    data() {
+      return {
+        articls: []
+      };
+    },
+    created() {
+      this.getArticles();
+    },
+    methods: {
+      getArticles() {
+        Data.get("/getArticle").then(res => {
+          this.articls = res.data;
+        });
+      }
+    }
+  };
 </script>
 
 <style>
-
 </style>
--- src/index/component/Products.vue
+++ src/index/component/Products.vue
@@ -1,13 +1,49 @@
 <template>
-  <div>产品详情页面</div>
+  <div>
+    <div class="class-item" v-for="cl in classify" :key="cl.ClassID">
+      <img width="30px" height="30px" :src="cl.ClassImg" alt />
+      <span>{{cl.ClassName}}</span>
+    </div>
+    <div v-for="cl in classify" :key="`${cl.ClassID}-prod`">
+      <span>{{cl.ClassName}}</span>
+      <div>
+        <span v-for="(prod,index) in cl.Products" :key="index">{{prod.ProdName}}</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+  import { Data } from "../../service";
+  export default {
+    data() {
+      return {
+        classify: []
+      };
+    },
+    created() {
+      this.getProducts();
+    },
+    methods: {
+      getProducts() {
+        let classPr = Data.get("/getClassify");
+        let prodsPr = Data.get("/getProdList");
+        Promise.all([classPr, prodsPr]).then(([classifys, products]) => {
+          let classProds = {};
+          products.data.forEach(prod => {
+            let pl = classProds[prod.Classify];
+            if (pl) pl.push(prod);
+            else classProds[prod.Classify] = [prod];
+          });
+          classifys.data.forEach(item => {
+            item.Products = classProds[item.ClassID] || [];
+          });
+          this.classify = classifys.data;
+        });
+      }
+    }
+  };
 </script>
 
 <style>
-
 </style>
